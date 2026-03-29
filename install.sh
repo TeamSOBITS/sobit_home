720bc6509e0c8e15d12ca2e182874b3e795e0612
MrKeith99 <kvalentincardenas@hotmail.com>
2026-03-29 18:18:21 +0900

    [UPD] hardware path

--- install.sh
+++ install.sh
@@ -105,9 +105,12 @@ sudo apt-get install -y \
 # Set up environment variables
 echo "" >> /home/$USERNAME/.bashrc
 echo "# SOBIT HOME environment variables" >> /home/$USERNAME/.bashrc
-echo "export DXL_X_LOWER_PORT=`realpath /dev/serial/by-id/usb-FTDI_USB__-__Serial_Converter_FT7W9E4N-if00-port0`" >> /home/$USERNAME/.bashrc
-echo "export DXL_X_UPPER_PORT=`realpath /dev/serial/by-id/usb-FTDI_USB__-__Serial_Converter_FT7W9E57-if00-port0`" >> /home/$USERNAME/.bashrc
-echo "export DXL_P_UPPER_PORT=`realpath /dev/serial/by-id/usb-FTDI_USB__-__Serial_Converter_FT4TCRFG-if00-port0`" >> /home/$USERNAME/.bashrc
+echo "export DXL_X_LOWER_PORT=\`realpath /dev/serial/by-id/usb-FTDI_USB__-__Serial_Converter_FT7W9E4N-if00-port0\`" >> /home/$USERNAME/.bashrc
+echo "export DXL_X_UPPER_PORT=\`realpath /dev/serial/by-id/usb-FTDI_USB__-__Serial_Converter_FT7W9E57-if00-port0\`" >> /home/$USERNAME/.bashrc
+echo "export DXL_P_UPPER_PORT=\`realpath /dev/serial/by-id/usb-FTDI_USB__-__Serial_Converter_FT4TCRFG-if00-port0\`" >> /home/$USERNAME/.bashrc
+echo "export UM_PORT=\`realpath /dev/serial/by-id/usb-Silicon_Labs_CP2102N_USB_to_UART_Bridge_Controller_dabce0b66407f0118d421f2e6d9880ab-if00-port0\`" >> /home/$USERNAME/.bashrc
+echo "export HOME_CAM_LEFT_PORT=\"/dev/\$(ls /sys/bus/usb/devices/3-5.3:1.0/video4linux/ | sort -V | head -1)\"" >> /home/$USERNAME/.bashrc
+echo "export HOME_CAM_RIGHT_PORT=\"/dev/\$(ls /sys/bus/usb/devices/3-6.3:1.0/video4linux/ | sort -V | head -1)\"" >> /home/$USERNAME/.bashrc
 echo "" >> /home/$USERNAME/.bashrc
 source /home/$USERNAME/.bashrc
 
