af7cb9699419eb424b578d61c4c5eade635ed932
MrKeith99 <kvalentincardenas@hotmail.com>
2026-04-06 19:05:11 +0900

    [DEL] not used launchers

--- sobit_home_moveit_config/launch/rsp.launch.py
+++
@@ -1,7 +0,0 @@
-from moveit_configs_utils import MoveItConfigsBuilder
-from moveit_configs_utils.launches import generate_rsp_launch
-
-
-def generate_launch_description():
-    moveit_config = MoveItConfigsBuilder("sobit_home", package_name="sobit_home_moveit_config").to_moveit_configs()
-    return generate_rsp_launch(moveit_config)
