2a3073b7e4d827e2864644c7b149bbd7227fe261
MrKeith99 <kvalentincardenas@hotmail.com>
2026-04-11 19:25:31 +0900

    [UPD] coding style

--- sobit_home_library/launch/action_server.launch.py
+++ sobit_home_library/launch/action_server.launch.py
@@ -9,16 +9,16 @@ from launch.substitutions import LaunchConfiguration
 
 
 def generate_launch_description():
-    arg_robot_name   = DeclareLaunchArgument('robot_name',    default_value='sobit_home')
-    arg_use_sim_time = DeclareLaunchArgument('use_sim_time',  default_value='true')
-    arg_use_rviz     = DeclareLaunchArgument('use_rviz',      default_value='true')
-    arg_enable_teleop   = DeclareLaunchArgument('enable_teleop',    default_value='false')
-    arg_linear_kp    = DeclareLaunchArgument('linear_kp',     default_value='0.1')
-    arg_linear_kd    = DeclareLaunchArgument('linear_kd',     default_value='0.4')
-    arg_linear_ki    = DeclareLaunchArgument('linear_ki',     default_value='0.8')
-    arg_rotate_kp    = DeclareLaunchArgument('rotate_kp',     default_value='0.1')
-    arg_rotate_kd    = DeclareLaunchArgument('rotate_kd',     default_value='0.4')
-    arg_rotate_ki    = DeclareLaunchArgument('rotate_ki',     default_value='0.8')
+    arg_robot_name    = DeclareLaunchArgument('robot_name',    default_value='sobit_home')
+    arg_use_sim_time  = DeclareLaunchArgument('use_sim_time',  default_value='true')
+    arg_use_rviz      = DeclareLaunchArgument('use_rviz',      default_value='false')
+    arg_enable_teleop = DeclareLaunchArgument('enable_teleop', default_value='false')
+    arg_linear_kp     = DeclareLaunchArgument('linear_kp',     default_value='0.1')
+    arg_linear_kd     = DeclareLaunchArgument('linear_kd',     default_value='0.4')
+    arg_linear_ki     = DeclareLaunchArgument('linear_ki',     default_value='0.8')
+    arg_rotate_kp     = DeclareLaunchArgument('rotate_kp',     default_value='0.1')
+    arg_rotate_kd     = DeclareLaunchArgument('rotate_kd',     default_value='0.4')
+    arg_rotate_ki     = DeclareLaunchArgument('rotate_ki',     default_value='0.8')
 
     return LaunchDescription([
         arg_robot_name,
@@ -36,16 +36,16 @@ def generate_launch_description():
 
 
 def launch_setup(context, *args, **kwargs):
-    robot_name   = LaunchConfiguration('robot_name').perform(context)
-    use_sim_time = LaunchConfiguration('use_sim_time').perform(context)
-    use_rviz     = LaunchConfiguration('use_rviz').perform(context)
-    enable_teleop   = LaunchConfiguration('enable_teleop').perform(context)
-    linear_kp    = float(LaunchConfiguration('linear_kp').perform(context))
-    linear_kd    = float(LaunchConfiguration('linear_kd').perform(context))
-    linear_ki    = float(LaunchConfiguration('linear_ki').perform(context))
-    rotate_kp    = float(LaunchConfiguration('rotate_kp').perform(context))
-    rotate_kd    = float(LaunchConfiguration('rotate_kd').perform(context))
-    rotate_ki    = float(LaunchConfiguration('rotate_ki').perform(context))
+    robot_name    = LaunchConfiguration('robot_name').perform(context)
+    use_sim_time  = LaunchConfiguration('use_sim_time').perform(context)
+    use_rviz      = LaunchConfiguration('use_rviz').perform(context)
+    enable_teleop = LaunchConfiguration('enable_teleop').perform(context)
+    linear_kp     = float(LaunchConfiguration('linear_kp').perform(context))
+    linear_kd     = float(LaunchConfiguration('linear_kd').perform(context))
+    linear_ki     = float(LaunchConfiguration('linear_ki').perform(context))
+    rotate_kp     = float(LaunchConfiguration('rotate_kp').perform(context))
+    rotate_kd     = float(LaunchConfiguration('rotate_kd').perform(context))
+    rotate_ki     = float(LaunchConfiguration('rotate_ki').perform(context))
 
     use_sim_time_bool = use_sim_time.lower() == 'true'
 
