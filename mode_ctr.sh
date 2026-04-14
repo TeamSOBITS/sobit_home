c0f1da9b27f756424b2886ad6d30e991329dbb5c
MrKeith99 <kvalentincardenas@hotmail.com>
2026-04-14 10:22:21 +0900

    [FIX] stop daemon after domain change

--- mode_ctr.sh
+++ mode_ctr.sh
@@ -1,9 +1,11 @@
 sobit_home_mode() {
     local domain_id=80
+    ros2 daemon stop
     export ROS_DOMAIN_ID=$domain_id
     echo "[SOBIT HOME] ROS_DOMAIN_ID=${ROS_DOMAIN_ID}"
 }
 default_mode() {
+    ros2 daemon stop
     export ROS_DOMAIN_ID=${HOST_ROS_DOMAIN_ID}
     echo "[Default] ROS_DOMAIN_ID=${ROS_DOMAIN_ID}"
 }
