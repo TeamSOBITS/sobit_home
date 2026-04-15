ee3fc492115303df417f00a7b5f1dd0b13299b16
Jiahao9 <simjiahao9@gmail.com>
2026-04-15 23:37:40 +0900

    [UPD] update camera info topic remappings for head and hand cameras

--- sobit_home_bringup/launch/robot.launch.py
+++ sobit_home_bringup/launch/robot.launch.py
@@ -3,13 +3,13 @@ import subprocess
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, ExecuteProcess, OpaqueFunction, IncludeLaunchDescription, RegisterEventHandler, TimerAction
+from launch.actions import DeclareLaunchArgument, ExecuteProcess, OpaqueFunction, IncludeLaunchDescription, RegisterEventHandler, TimerAction, GroupAction
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.event_handlers import OnProcessExit, OnProcessStart
 from launch.substitutions import PathJoinSubstitution, LaunchConfiguration
 from launch.conditions import IfCondition
 from launch_ros.substitutions import FindPackageShare
-from launch_ros.actions import Node
+from launch_ros.actions import Node, SetRemap
 
 import xacro
 
@@ -526,9 +526,12 @@ def launch_gz(context, *args, **kwargs):
                     # "/" + robot_name + "/imu" + "@sensor_msgs/msg/Imu" + "[gz.msgs.IMU",
                 ],
         remappings=[
+                    ("/" + robot_name + "/head_camera/camera_info", "/" + robot_name + "/head_camera/color/camera_info"),
                     ("/" + robot_name + "/head_camera/color", "/" + robot_name + "/head_camera/color/image_raw"),
                     ("/" + robot_name + "/head_camera/depth", "/" + robot_name + "/head_camera/depth/image_raw"),
+                    ("/" + robot_name + "/hand_left_camera/camera_info", "/" + robot_name + "/hand_left_camera/color/camera_info"),
                     ("/" + robot_name + "/hand_left_camera/color", "/" + robot_name + "/hand_left_camera/color/image_raw"),
+                    ("/" + robot_name + "/hand_right_camera/camera_info", "/" + robot_name + "/hand_right_camera/color/camera_info"),
                     ("/" + robot_name + "/hand_right_camera/color", "/" + robot_name + "/hand_right_camera/color/image_raw"),
                 ],
         output='screen'
@@ -548,6 +551,7 @@ def launch_gz(context, *args, **kwargs):
                 ],
             remappings=[
                 ('image_raw', 'color/image_raw'),
+                ('camera_info', 'color/camera_info'),
                 ('image_raw/compressed', 'color/image_raw/compressed'),
                 ('image_raw/compressedDepth', 'color/image_raw/compressedDepth'),
                 ('image_raw/theora', 'color/image_raw/theora'),
@@ -568,6 +572,7 @@ def launch_gz(context, *args, **kwargs):
             ],
             remappings=[
                 ('image_raw', 'color/image_raw'),
+                ('camera_info', 'color/camera_info'),
                 ('image_raw/compressed', 'color/image_raw/compressed'),
                 ('image_raw/compressedDepth', 'color/image_raw/compressedDepth'),
                 ('image_raw/theora', 'color/image_raw/theora'),
