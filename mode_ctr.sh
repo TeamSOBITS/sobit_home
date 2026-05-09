88930792b47f3977e378f989c7718b812539cb55
satokeeen <kenta.sato5044@gmail.com>
2026-05-09 17:20:21 +0900

    [FIX]switch CYCLONEDDS_URI via mode_ctr

--- mode_ctr.sh
+++ mode_ctr.sh
@@ -1,10 +1,12 @@
 sobit_home_mode() {
+    export CYCLONEDDS_URI=${REMOTE_CYCLONEDDS_URI}
     local domain_id=80
     ros2 daemon stop
     export ROS_DOMAIN_ID=$domain_id
     echo "[SOBIT HOME] ROS_DOMAIN_ID=${ROS_DOMAIN_ID}"
 }
 default_mode() {
+    unset CYCLONEDDS_URI
     ros2 daemon stop
     export ROS_DOMAIN_ID=${HOST_ROS_DOMAIN_ID}
     echo "[Default] ROS_DOMAIN_ID=${ROS_DOMAIN_ID}"
